e130a236bdd81296a5a2e5045887cd89645c9da1
anblo <anblo@LAPTOP-B8H4HR2>
2025-07-06 22:52:01 +0200

    started on day 7!

--- day07/day07.cpp
+++ day07/day07.cpp
@@ -2,14 +2,71 @@
 #include <chrono>
 #include <iostream>
 #include "day07.h"
+#include "../Common/utils.h"
+#include <cmath>
 
 namespace day07{
     using namespace std;
     int solve_A(string input){
         int ans = 0;
+        vector<string> lines = utils::ReadAllLines(input);
+
+        long long value;
+        vector<int> values;
+        string tempNumbers;
+        vector<vector<int>> numbers;
+        int i=0;
+        for (string line : lines)
+        {
+            vector<int> v;
+            cout << line << endl;
+            int index = line.find(":");
+            value = stoll(line);
+            values.push_back(value);
+
+            tempNumbers = line.substr(index+2);
+            while(tempNumbers.find(" ") != string::npos){
+                v.push_back(stoi(tempNumbers));
+                index = tempNumbers.find(" ");
+                tempNumbers = tempNumbers.substr(index+1);
+            }
+            v.push_back(stoi(tempNumbers));
+            numbers.push_back(v);
+            i++;
+        }
+        cout << endl;
+        
+        for(int i=0; i < values.size(); i++){
+            vector<int> numbersToCheck = numbers[i];
+            value = values[i];
+            //cout << "checking value: " << value << ", with numbers ->" << numbersToCheck[0] << endl;
+
+            int num_of_variations = pow(2,numbersToCheck.size()-1);
+            for(int i = 0; i<num_of_variations; i++){
+                if(value == applyOps(numbersToCheck, i)){
+                    ans += value;
+                    cout << "found matching comb!, adding value: " << value << endl;
+                    break;
+                } 
+            }
+        }
 
         return ans;
     }
+
+    long long applyOps(const vector<int> &nums, int mask){
+        long long result = nums[0];
+        for(int i =1; i < nums.size(); i++){
+            if((mask >> i-1) & 1){
+                result *= nums[i];
+            } else {
+                result += nums[i];
+            }
+        }
+        return result;
+    }
+
+
 
     int solve_B(string input){
         int ans = 0;
--- day07/day07.h
+++ day07/day07.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <string>
+#include <vector>
 
 std::string INPUT_DAY_7 = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day07.txt";
 std::string INPUT_DAY_7_example = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day07_example.txt";
+
+namespace day07{
+    long long applyOps(const std::vector<int> &nums, int mask);
+}
