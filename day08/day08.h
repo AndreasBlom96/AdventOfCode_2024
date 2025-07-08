dc300e556304432743d3dbc70945b7421413f9f3
anblo laptop <anblo@LAPTOP-B8H4HR2>
2025-07-08 21:09:34 +0200

    day8 setup

---
+++ day08/day08.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include <vector>
+
+std::string INPUT_DAY_8 = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day08.txt";
+std::string INPUT_DAY_8_example = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day08_example.txt";
