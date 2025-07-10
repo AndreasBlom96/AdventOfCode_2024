e8768668f1b6f51ebd3cb096a2380bfb27372672
anblo laptop <anblo@LAPTOP-B8H4HR2>
2025-07-10 23:32:21 +0200

    started day9 A

---
+++ day09/day09.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+#include<iostream>
+
+std::string INPUT_DAY_09 = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day09.txt";
+std::string INPUT_DAY_09_example = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day09_example.txt";
+
+namespace day09{
+    int solve_A(std::string input);
+    int solve_B(std::string input);
+}
