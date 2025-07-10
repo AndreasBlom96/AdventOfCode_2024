2690c9d2a88d36eb616b61e4a2852201e01ce12c
anblo laptop <anblo@LAPTOP-B8H4HR2>
2025-07-10 23:11:08 +0200

    nearly there

---
+++ Script/AoC_Setup.py
@@ -0,0 +1,109 @@
+import os
+from download_input import fetch_input_data
+
+
+
+def CMake_text(day):
+    CMake_list_text = f""" add_executable(day{day:02} day{day:02}.cpp)
+
+target_link_libraries(day{day:02} PRIVATE common) """
+    return CMake_list_text
+
+
+def setup(day):
+    #make directory
+    new_dir = project_path + f"/day{day:02}"
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+        print("directory does not exits, creates new dir")
+    else:
+        print("directory already exists!")
+    cpp_file = project_path + f"/day{day:02}/day{day:02}.cpp"
+    h_file = project_path + f"/day{day:02}/day{day:02}.h"
+    Cmake_file = project_path + f"/day{day:02}/CMakeLists.txt"
+
+    root_CMake_line_text = f"add_subdirectory(day{day:02})"
+
+    #make cpp file
+    make_cpp_file(cpp_file, day)
+
+    #make h file
+    make_h_file(h_file, day)
+
+    make_CMake(CMake_text(day), Cmake_file)
+    root_CMake_line(root_CMake_line_text, project_path + f"/CMakeLists.txt")
+    fetch_input_data(day)
+
+def root_CMake_line(line, path):
+    print("writing new line to root CMakeLists.txt")
+    with open(path, "r+") as CMake_file:
+        for row in CMake_file:
+            pass
+        last_line = row
+        if line == last_line:
+            return
+        CMake_file.write("\n")
+        CMake_file.write(line)
+
+def read_template(path, day):
+    with open(path, "r") as file:
+        content = file.read()
+    return content.replace("?", f"{day:02}")
+
+
+def make_cpp_file(path, day):
+    template_path = "C:/Users/anblo/Documents/CodingProjects/AoC_Uni/Script/template/template_main_cpp.txt"
+    text = read_template(template_path, day)
+
+    print("creating cpp file")
+    if os.path.exists(path):
+        return
+    with open(path,"w") as cppFile:
+        cppFile.write(text)
+
+def make_h_file(path, day):
+    template_path = "C:/Users/anblo/Documents/CodingProjects/AoC_Uni/Script/template/template_h.txt"
+    text = read_template(template_path, day)
+
+    print("creating .h file")
+    if os.path.exists(path):
+        return
+    with open(path, "w") as hFile:
+        hFile.write(text)
+
+def make_CMake(text, path):
+    print("creating CMakeLists for the day")
+    if os.path.exists(path):
+        return
+    with open(path, "w") as CMakeFile:
+        CMakeFile.write(text)
+
+if __name__== "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv)
+
+
+    #set new working dir
+    project_path = "C:/Users/anblo/Documents/CodingProjects/AoC_Uni"
+    os.chdir(project_path)
+    print(project_path)
+
+    if argc > 1:
+        if argv[1] == "help":
+            print(" specify day!")
+        day = int(argv[1])
+        if day < 25 and day > 0:
+            print(f"setting up day {day:02}")
+            setup(day)
+        else:
+            print("day must be between 0 and 25")
+
+
+
+
+
+
+
+
+
--- Script/download_input.py
+++ Script/download_input.py
@@ -1,18 +1,48 @@
 import requests
 import time
+import os
 
-day = 8
-year = 2024
 
-session_cookie = "53616c7465645f5f82eb2a130147026a21a12b32d641ea6d6717d623b63fdf38d8518ff55feee89b724d72c954c203a56c6cacc661a19eef3ba62fd35599e403"
-url = f"https://adventofcode.com/{year}/day/{day}/input"
+def fetch_input_data(day):
+    year = 2024
+    print(f"fetching input data for day:{day:02} , year: {year}")
+    session_cookie = "53616c7465645f5f82eb2a130147026a21a12b32d641ea6d6717d623b63fdf38d8518ff55feee89b724d72c954c203a56c6cacc661a19eef3ba62fd35599e403"
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
 
-response = requests.get(url, cookies={"session": session_cookie})
+    #check if the path has a size > 0 OR  even exists OR has the same text i want to copy in
+    input_path = f"C:/Users/anblo/Documents/CodingProjects/AoC_Uni/inputs/day{day:02}.txt"
+    example_path = f"C:/Users/anblo/Documents/CodingProjects/AoC_Uni/inputs/day{day:02}_example.txt"
+    #input_path = f"A/inputs/day{day:02}.txt"
+    if os.path.exists(input_path):
+        print(f"input txt file for day {day:02} already exists! want to continue anyway? .....")
+        ans = input("y/n:  ")
+        if ans!="Y":
+            print("exiting...")
+            return
+    
 
-if response.ok:
-    #write code here
-    print("session ok!")
-    with open(f"inputs/day{day:02}.txt", "w") as file:
-        file.write(response.text)
-else:
-    print("session failed!") 
+    response = requests.get(url, cookies={"session": session_cookie})
+    if response.ok:
+        #print("session ok!")
+        with open(input_path, "w") as file:
+            file.write(response.text)
+    else:
+        print("session failed!")
+
+    with open(example_path, "w") as exFile:
+        exFile.write("")
+
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv)
+
+    if argc > 1:
+        if argv[1] == "help":
+            print(" specify day!")
+        day = int(argv[1])
+        if day < 25 and day > 0:
+            fetch_input_data(day)
+        else:
+            print("day must be between 0 and 25")
