89f90e9a6df8b2d9edb87da4c35dc10489b4db83
anblo laptop <anblo@LAPTOP-B8H4HR2>
2025-07-15 11:18:24 +0200

    Setup for day 11

---
+++ day11/day11.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+#include<iostream>
+
+std::string INPUT_DAY_11 = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day11.txt";
+std::string INPUT_DAY_11_example = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day11_example.txt";
+
+namespace day11{
+    int solve_A(std::string input);
+    int solve_B(std::string input);
+}
