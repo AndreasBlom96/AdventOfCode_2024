e245cd69a8e9213897c5ab5469d44b0b156b1674
AndreasBlom96 <60653859+AndreasBlom96@users.noreply.github.com>
2025-07-22 21:47:30 +0200

    Day13 a (#9)
    * setup and start of day13A
    
    * cleared day13A
    
    ---------
    
    Co-authored-by: anblo laptop <anblo@LAPTOP-B8H4HR2>

---
+++ day13/day13.cpp
@@ -0,0 +1,195 @@
+#include <string>
+#include "../common/utils.h"
+#include <iostream>
+#include <chrono>
+#include "day13.h"
+
+class machine{
+    private:
+    std::pair<int,int> button_a;
+    std::pair<int,int> button_b;
+    std::pair<int,int> prize;
+
+    public:
+
+    machine(std::pair<int,int> a, std::pair<int,int> b, std::pair<int,int> prize) 
+    : button_a(a), button_b(b), prize(prize){}
+
+    unsigned long long getCost(int numA, int numB) const {
+        return 3*numA + numB;
+    }
+
+    bool checkPrize(int numA, int numB) const {
+        return (numA * button_a.first + numB * button_b.first == prize.first) && 
+        (numA * button_a.second + numB * button_b.second == prize.second);
+    }
+
+    std::string getStarter(){
+        std::string result;
+        if(prize.first > prize.second){
+            result = "f";
+        } else {
+            result = "s";
+        }
+
+        if(result == "f"){
+            if(button_a.first > 3*button_b.first){
+                result += "a";
+            } else {
+                result += "b";
+            }
+        } else if (result == "s")
+        {
+            if(button_a.second > 3*button_b.second){
+                result += "a";
+            } else {
+                result += "b";
+            }
+        }
+        return result;
+    }
+
+    std::pair<int,bool> find_solution(){
+
+        //four crucial test. it seems like it doesnt matter which test i do? i get same answer every time?
+        auto [first, second] = helper_solution(button_a.first, button_b.first, prize.first, true);
+        int costA = getCost(first, second);
+        /*
+        auto [a, b] = helper_solution(button_b.first, button_a.first, prize.first, false);
+        int costB = getCost(a, b);
+        auto [firstB, secondB] = helper_solution(button_a.second, button_b.second, prize.second, true);
+        int costC = getCost(firstB, secondB);
+        auto [aB, bB] = helper_solution(button_b.second, button_a.second, prize.second, false);
+        int costD = getCost(aB, bB);
+        */
+        if(first == -1) return std::make_pair(-1, false);
+
+
+        return std::make_pair(costA,true);
+    }
+
+    std::pair<int,int> helper_solution(int first, int second, int prize, bool A_is_first){
+        int start = prize % first;
+        int first_multiplier = prize / first;
+        if(first_multiplier > 100){
+            first_multiplier = 100;
+            start = prize - first_multiplier * first;
+        }
+        while(first_multiplier >= 0){
+            if(start % second == 0){
+                // found matching, I DONT KNOW WHAT IS A AND B???
+                int second_multiplier = start / second;
+                if(A_is_first){
+                    if(checkPrize(first_multiplier, second_multiplier))
+                    {
+                        return std::make_pair(first_multiplier, second_multiplier);
+                    }  
+                } else {
+                    if (checkPrize(second_multiplier, first_multiplier))
+                    {
+                        return std::make_pair(second_multiplier, first_multiplier);
+                    }
+                    
+                }
+                
+            }
+            start += first;
+            first_multiplier--;
+        }
+        return std::make_pair(-1,-1);
+    }
+
+};
+
+
+namespace day13{
+    using namespace std;
+    unsigned long long solve_A(string input){
+        unsigned long long ans = 0;
+        vector<string> lines = utils::ReadAllLines(input);
+
+
+        pair<int,int> A;
+        pair<int,int> B;
+        pair<int,int> p;
+        vector<machine> machines;
+        for(string line : lines){
+            if(line.find("A:") != string::npos){
+                A.first = stoi(line.substr(line.find("+") +1));
+                A.second = stoi(line.substr(line.rfind("+") +1));
+            } else if (line.find("B:") != string::npos)
+            {
+                B.first = stoi(line.substr(line.find("+") +1));
+                B.second = stoi(line.substr(line.rfind("+") +1));
+            } else if (line.find("Prize:") != string::npos)
+            {
+                p.first = stoi(line.substr(line.find("=") +1));
+                p.second = stoi(line.substr(line.rfind("=") +1));
+                machine m = machine(A,B,p);
+                machines.push_back(m);
+            }
+        }
+        
+        pair<int,bool> result;
+        for(machine m : machines){
+            result = m.find_solution();
+            if(result.second){
+                //cout << "found solution: price -> " << result.first << endl;
+                ans += result.first; 
+            }
+        }
+        return ans;
+    }
+
+    unsigned long long solve_B(string input){
+        unsigned long long ans = 0;
+
+        return ans;
+    }
+
+}
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[]){
+    std::string input = INPUT_DAY_13;
+    unsigned long long result;
+    if (argc >= 3 and std::string(argv[2]) == "example")
+    {
+        input = INPUT_DAY_13_example;
+    }
+    
+    if (argc >= 2)  
+    {
+        std::string part = argv[1];
+        if (part == "A")
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            result = day13::solve_A(input);
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            std::cout << "it took: " << duration.count() << "ms" << std::endl;
+            std::cout << "answer is -> " << result << std::endl;
+            return result;
+        }
+        else if (part == "B")
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            result = day13::solve_B(input);
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            std::cout << "it took: " << duration.count() << "ms" << std::endl;
+            std::cout << "answer is -> " << result << std::endl;
+            return result;
+        }
+        
+        std::cout << "unknown command" << std::endl;
+        return -1;
+    }
+    
+}
---
+++ day13/day13.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+std::string INPUT_DAY_13 = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day13.txt";
+std::string INPUT_DAY_13_example = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day13_example.txt";
+
+namespace day13{
+    unsigned long long solve_A(std::string input);
+    unsigned long long solve_B(std::string input);
+}
