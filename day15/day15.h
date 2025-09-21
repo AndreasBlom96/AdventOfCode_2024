2734dfceabeca8d27d4112e13073877c09b0a08b
anblo laptop <anblo@LAPTOP-B8H4HR2>
2025-09-21 12:23:51 +0200

    start of day 15

---
+++ day15/day15.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+std::string INPUT_DAY_15 = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day15.txt";
+std::string INPUT_DAY_15_example = "C:\\Users\\anblo\\Documents\\CodingProjects\\AoC_Uni\\inputs\\day15_example.txt";
+
+namespace day15{
+    int solve_A(std::string input);
+    int solve_B(std::string input);
+}
